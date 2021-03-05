9106b7dea3706134c7412f1cd8b20c02b2cbb35a
stuchalk <schalk@unf.edu>
2021-03-05 09:12:07 -0500

    fixes for lint issues in users app

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,16 @@
+"""user app forms config"""
 from django import forms
+# TODO needed?
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class RegisterForm(UserCreationForm):
+    """user registration form"""
     email = forms.EmailField()
 
     class Meta:
+        """metadata"""
         model = User
-        fields = ["username", "email", "password1","password2"]
-
+        fields = ["username", "email", "password1", "password2"]
--- users/models.py
+++ users/models.py
@@ -1,9 +1,13 @@
+"""user app model file"""
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
 
 class Request(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='user', default='', blank=True, null=True)
+    """model for Django users table"""
+    user = models.ForeignKey(User, on_delete=models.PROTECT,
+                             related_name='user', default='',
+                             blank=True, null=True)
     type = models.CharField(max_length=200)
     action = models.CharField(max_length=200)
     content = models.CharField(max_length=200)
