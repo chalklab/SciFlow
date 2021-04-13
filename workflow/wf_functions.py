f49dd97db85a74e07c51216bb7cf3387c62ea2d7
stuchalk <schalk@unf.edu>
2021-04-13 07:06:51 -0400

    more lint corrections

--- workflow/wf_functions.py
+++ workflow/wf_functions.py
@@ -27,10 +27,7 @@ def ingest(upload, user):
         ids = updatedatafile(file)
         gvars.ingest_data_lookup_id = ids['mid']
         gvars.ingest_data_file_id = ids['fid']
-        actlog(
-            "WF_A02: UID is " +
-            JsonLookup.objects.get(
-                id=ids['mid']).uniqueid)
+        actlog("WF_A02: UID is " + JsonLookup.objects.get(id=ids['mid']).uniqueid)
 
         sections = {}
         # this would be expanded as we get me code written for other unique
@@ -52,11 +49,8 @@ def ingest(upload, user):
 
         if sections:
             actlog("WF_A03: Sections: " + str(sections.keys()))
-            if normalize(
-                    file,
-                    sections,
-                    user,
-                    ids['mid']) is True:  # normalization.py
+            # normalization.py
+            if normalize(file, sections, user, ids['mid']) is True:
                 actlog("WF_A04: File normalized!")
                 return True
             else:
@@ -93,30 +87,20 @@ def normalize(dfile, sections, user, jl):
                         # sd file and add to both
                         ffile = createsubjld(subid)
                         # add facet file to DB
-                        maxid = FacetLookup.objects.all().aggregate(
-                            Max('id'))['id__max']
+                        maxid = FacetLookup.objects.all().aggregate(Max('id'))['id__max']
                         nextid = maxid + 1 if maxid else 1
-                        fid = str(nextid).rjust(
-                            8, '0')  # creates id with the right length
+                        fid = str(nextid).rjust(8, '0')  # creates id with the right length
                         ffile['@id'] = "https://scidata.unf.edu/facet/" + fid
                         ffileid = addfacetfile(ffile, user)
                         if not ffileid:
-                            errorlog(
-                                "WF_E05: Compound file metadata for "
-                                "substance id " + str(subid)
-                                + " not added to facet_lookup")
+                            errorlog("WF_E05: Compound file metadata for substance id " + str(subid) + " not added to facet_lookup")
                         if not updatefacetfile(ffile):
-                            errorlog("WF_E06: Compound file id "
-                                     + str(ffileid)
-                                     + " was not added to facet_files")
+                            errorlog("WF_E06: Compound file id " + str(ffileid) + " was not added to facet_files")
                         # now that facet file has be added link to DB table
                         updatesubstance(subid, 'facet_lookup_id', ffileid)
-                        actlog(
-                            "WF_A06: Created compound facet file id "
-                            + str(ffileid) + " and added to DB")
+                        actlog("WF_A06: Created compound facet file id " + str(ffileid) + " and added to DB")
                     if not graphid:
-                        namedgraph = 'https://scidata.unf.edu/facet/' \
-                                     + str(ffileid)
+                        namedgraph = 'https://scidata.unf.edu/facet/' + str(ffileid)
                         # has the jsonld file been saved in the DB but not
                         # added to the graph?
                         if addgraph('facet', ffileid, 'remote', namedgraph):
@@ -124,13 +108,9 @@ def normalize(dfile, sections, user, jl):
                             sub = Substances.objects.get(id=subid)
                             sub.graphdb = namedgraph
                             sub.save()
-                            actlog(
-                                "WF_A07: Compound file id "
-                                + str(ffileid) + " added to GraphDB")
+                            actlog("WF_A07: Compound file id " + str(ffileid) + " added to GraphDB")
                         else:
-                            errorlog(
-                                "WF_E07: Compound file id " + str(ffileid)
-                                + " was not added to GraphDB")
+                            errorlog("WF_E07: Compound file id " + str(ffileid) + " was not added to GraphDB")
 
                     # load facet file to extract @id for compound
                     fobjt = FacetFiles.objects.get(facet_lookup_id=ffileid)
@@ -156,13 +136,9 @@ def normalize(dfile, sections, user, jl):
                     link.json_lookup_id = jl
                     link.facets_lookup_id = ffileid
                     link.save()
-                    actlog(
-                        "WF_A08: Compound found in DB: ( "
-                        + str(section) + ", file id " + str(ffileid) + " )")
+                    actlog("WF_A08: Compound found in DB: ( " + str(section) + ", file id " + str(ffileid) + " )")
                 else:
-                    errorlog(
-                        "WF_E08: Compound not found in or added to DB ( "
-                        + str(section) + ", " + str(entry) + " )")
+                    errorlog("WF_E08: Compound not found in or added to DB ( " + str(section) + ", " + str(entry) + " )")
 
     # update file in DB
     updated = updatedatafile(dfile, 'normalized')
@@ -170,13 +146,10 @@ def normalize(dfile, sections, user, jl):
         atid = dfile['@id']
         parts = atid.split('/')
         if addgraph('data', parts[4], 'remote', dfile['@id']):
-            actlog(
-                "WF_A09: Normalized version of datafile added to graph: "
-                + str(parts[4]))
+            actlog("WF_A09: Normalized version of datafile added to graph: " + str(parts[4]))
             return True
         else:
-            errorlog(
-                "WF_E09: Normalized version of datafile not saved in graph")
+            errorlog("WF_E09: Normalized version of datafile not saved in graph")
         return True
 
 
