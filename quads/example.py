6fe71c0e06c16bc914d5cb72b95004ac518b363e
stuchalk <schalk@unf.edu>
2021-07-05 06:13:58 -0400

    updates to quads functionality

--- quads/example.py
+++ quads/example.py
@@ -10,20 +10,30 @@ django.setup()
 from django.conf import settings
 from datafiles.views import *
 from pyld import jsonld
-from quads.qd_functions import add
+from quads.qd_functions import *
 
 
 # function to grab datafiles from the sciflow database (using the PHP sciflow interface) and save in quads table
 # dlist 1 (herg), 3 (trc)
 f1 = True
 if f1:
-    fobjs = requests.get('https://sds.coas.unf.edu/sciflow/files/dlist/3/23265')
-    flist = fobjs.json()
-    for url in flist:
+    setobjs = requests.get('https://sds.coas.unf.edu/trc/datasets/sddslist/54072')
+    setlist = setobjs.json()
+    # done = Quads.objects.order_by('obj').values_list('obj', flat=True).distinct()
+    for fname, url in setlist.items():
         opts = {'algorithm': 'URDNA2015', 'format': 'application/n-quads', 'processingMode': 'json-ld-1.1'}
         normalized = jsonld.normalize(url, opts)
         quads = normalized.split('\n')
-        for quad in quads:
-            add(quad)
-        print('Processed ' + url)
-    exit()
+        for qidx, quad in enumerate(quads):
+            if quad == '':
+                quads.pop(qidx)
+                continue
+            q = quad.replace(' .', '').replace('> ', '>@@@').replace('" <', '"@@@<').split('@@@')
+            if len(q) == 3:
+                q.append(None)
+            elif len(q) > 4:
+                print(q)
+                exit()
+            quads[qidx] = q
+        addbulk(quads)
+        print('Processed ' + fname + ':' + url)
--- quads/qd_functions.py
+++ quads/qd_functions.py
@@ -18,5 +18,20 @@ def add(quad):
     if len(q) == 4:
         Quads.objects.get_or_create(sub=q[0], prd=q[1], obj=q[2], gph=q[3])
     elif len(q) == 3:
-        Quads.objects.get_or_create(sub=q[0], prd=q[1], obj=q[2])
+        Quads.objects.get_or_create(sub=q[0], prd=q[1], obj=q[2], gph=None)
+    return
+
+
+def find(type,text):
+    filter_kwargs = { "{}__icontains".format(type): text }
+    hits = Quads.objects.values(type).filter(**filter_kwargs)
+    return hits
+
+
+def addbulk(quads):
+    objs = [
+        Quads(sub=q[0], prd=q[1], obj=q[2], gph=q[3])
+        for q in quads
+    ]
+    Quads.objects.bulk_create(objs)
     return
