78a5fb740e9724505d87435852c89d480e3eee76
stuchalk <schalk@unf.edu>
2023-01-03 12:15:12 -0500

    update settings so include localsettings

--- sciflow/settings.py
+++ sciflow/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from sciflow.localsettings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -113,8 +114,8 @@ UNFDB = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sciflow',
-        'USER': 'sciflow',
-        'PASSWORD': 'letthedatafl0w',
+        'USER': sfuser,
+        'PASSWORD': sfpass,
         'HOST': '127.0.0.1',
         'PORT': '3307'
     },
@@ -129,7 +130,6 @@ UNFDB = {
 }
 
 DATABASES = UNFDB
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
